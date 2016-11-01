fff0f7438914239c77062bbb3179a390c4cabeb7
manhhung.dt6 <manhhung.dt6@gmail.com>
2016-11-01 11:16:44 +0700

    update

--- PushPop/ViewController.swift
+++ PushPop/ViewController.swift
@@ -11,12 +11,12 @@ import UIKit
 class ViewController: UIViewController {
     @IBOutlet weak var logo_img: UIImageView!
     @IBOutlet weak var lbl_text: UILabel!
-    @IBOutlet weak var lbl_start: UIButton!
+    // @IBOutlet weak var lbl_start: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         logo_img!.alpha = 0
         lbl_text!.alpha = 0
-        lbl_start!.alpha = 0
+        // lbl_start!.alpha = 0
     }
 
     override func viewWillAppear(_ animated: Bool) {
@@ -28,18 +28,20 @@ class ViewController: UIViewController {
                     self.lbl_text!.center = CGPoint(x: self.logo_img!.center.x, y: 350)
                     self.lbl_text!.alpha = 1
                 }, completion: { (finished) in
-                    UIView.animate(withDuration: 1, animations: {
+                    UIView.animate(withDuration: 3, animations: {
                     self.lbl_text!.alpha = 0
 //                    self.lbl_start!.center = CGPoint(x: self.logo_img!.center.x, y: 600)
-                    self.lbl_start!.alpha = 1
+                    // self.lbl_start!.alpha = 1
+                    let v2 = self.storyboard?.instantiateViewController(withIdentifier: "V2") as! View2
+                    self.navigationController?.pushViewController(v2, animated: true)
                 })
             })
         })
     }
     
-    @IBAction func startPush(_ sender: AnyObject) {
-        let v2 = self.storyboard?.instantiateViewController(withIdentifier: "V2") as! View2
-        self.navigationController?.pushViewController(v2, animated: true)
-    }
+//    @IBAction func startPush(_ sender: AnyObject) {
+//        let v2 = self.storyboard?.instantiateViewController(withIdentifier: "V2") as! View2
+//        self.navigationController?.pushViewController(v2, animated: true)
+//    }
 }
 
